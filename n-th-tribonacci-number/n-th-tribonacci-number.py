6501fc08d62dbc62e17f835685e8b5b383f8cd20
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2021-09-20 18:47:32 -0400

    Time: 24 ms (95.57%), Space: 14.3 MB (70.17%) - LeetHub

--- n-th-tribonacci-number/n-th-tribonacci-number.py
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -7,4 +7,4 @@ class Solution:
         fib = [0,1,1]
         for i in range(3,n+1):
             fib.append(fib[i-1] + fib[i-2] + fib[i-3])
-        return fib[n] # n-th-tribonacci-number
+        return fib[n]
