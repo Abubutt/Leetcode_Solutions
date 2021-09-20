555b265b10b5e3b6781708dbd8a28536a1ea484b
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2021-09-20 18:44:06 -0400

    Time: 61 ms (5.66%), Space: 14.1 MB (70.17%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n<1:
+            return 0
+        elif n in [1,2]:
+            return 1
+        fib = [0,1,1]
+        for i in range(3,n+1):
+            fib.append(fib[i-1] + fib[i-2] + fib[i-3])
+        return fib[n]
