59c25c5bc34b0c25b63ad852ab0f16d5933718b2
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2022-04-19 21:28:42 -0400

    Time: 122 ms (64.23%), Space: 19.6 MB (14.28%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,18 +1,35 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        numDict = defaultdict(int)
-        ans = []
-        for num in nums:
-            if num in numDict:
-                numDict[num] += 1
-            else:
-                numDict[num] = 0
-        numDict = sorted(numDict.items(), key=lambda x: x[1], reverse=True)
+        # 0(nlogn)
+        # numDict = defaultdict(int)
+        # ans = []
+        # for num in nums:
+        #     if num in numDict:
+        #         numDict[num] += 1
+        #     else:
+        #         numDict[num] = 0
+        # numDict = sorted(numDict.items(), key=lambda x: x[1], reverse=True)
+        # print(numDict)
+        # for key,val in numDict:
+        #     if k == 0:
+        #         break
+        #     else:
+        #         ans.append(key)
+        #     k-=1
+        # return ans
         
-        for key,val in numDict:
-            if k == 0:
-                break
-            else:
-                ans.append(key)
-            k-=1
-        return ans
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+        
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+        for key,val in count.items():
+            freq[val].append(key)
+            
+        ans = []
+        
+        for i in range(len(freq) - 1, 0, -1):
+            for j in freq[i]:
+                ans.append(j)
+                if len(ans) == k:
+                    return ans
