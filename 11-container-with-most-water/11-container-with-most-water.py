50b71280655368fe81eccf68880c51cde7f4f4b9
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2022-04-26 13:24:24 -0400

    Time: 1598 ms (5.01%), Space: 27.5 MB (58.31%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -4,15 +4,14 @@ class Solution:
         end = len(height) - 1
         maxWater = 0
         
-        while start < end:
-            L = min(height[start], height[end])
-            W = end-start
-            currentArea = L*W
-            maxWater = max(maxWater, currentArea)
+        while start < end: # 0(n)
+            length = min(height[start], height[end]) # 0(2)
+            width = end-start
+            currentArea = length*width
+            maxWater = max(maxWater, currentArea) # 0(2)
             if height[start] > height[end]:
                 end-=1
             else:
                 start+=1
         return maxWater
-    
-    # 
+    # time complexity: 0(4n) --> 0(n)
