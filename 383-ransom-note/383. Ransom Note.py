5382baf27f9f784dfc7653c4f6b920986716c109
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2024-12-21 17:19:44 -0500

    Update 383. Ransom Note.py

--- 383-ransom-note/383. Ransom Note.py
+++ 383-ransom-note/383. Ransom Note.py
@@ -1,18 +1,30 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        countNote = defaultdict(int)
+#         countNote = defaultdict(int)
+#         countMag = defaultdict(int)
+        
+#         for char in ransomNote:
+#             countNote[char] += 1
+            
+#         for char in magazine:
+#             countMag[char] += 1
+        
+#         for key in countNote:
+#             if countMag[key] < countNote[key]:
+#                 return False
+        
+#         return True
+          
         countMag = defaultdict(int)
         
-        for char in ransomNote:
-            countNote[char] += 1
-            
         for char in magazine:
             countMag[char] += 1
-        
-        for key in countNote:
-            if countMag[key] < countNote[key]:
+            
+        for char in ransomNote:
+            if countMag[char] <= 0:
                 return False
-        
+            countMag[char] -= 1
+            
         return True
             
         
