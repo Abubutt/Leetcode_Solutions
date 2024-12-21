6687cb7e8fdbc8d959e8b4d79a58155ddb044f9f
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2024-12-21 17:15:35 -0500

    Runtime: 19 ms (60.59%), Memory: 18.1 MB (5.78%) - Gitcode

---
+++ 383-ransom-note/383. Ransom Note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        countNote = defaultdict(int)
+        countMag = defaultdict(int)
+        
+        for char in ransomNote:
+            countNote[char] += 1
+            
+        for char in magazine:
+            countMag[char] += 1
+        
+        for key in countNote:
+            if countMag[key] < countNote[key]:
+                return False
+        
+        return True
+            
+        
