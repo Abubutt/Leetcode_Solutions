745481ee6ffc095df2d9f124f56397083928807f
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2024-12-20 19:34:50 -0500

    Update 268. Missing Number.py

--- 268-missing-number/268. Missing Number.py
+++ 268-missing-number/268. Missing Number.py
@@ -1,8 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:    
-        numsSet = set(nums)
+#         numsSet = set(nums)
         
-        for i in range(0, len(nums) + 1):
-            if i not in numsSet:
-                return i
+#         for i in range(0, len(nums) + 1):
+#             if i not in numsSet:
+#                 return i
         
+        size = len(nums)
+        
+        requiredSum = (size * (size + 1)) // 2
+        currentSum = sum(nums)
+        
+        return requiredSum - currentSum
