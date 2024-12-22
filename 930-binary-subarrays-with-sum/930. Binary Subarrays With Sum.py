6e8899178fa7f249311f9b735f869d35e0fa25b7
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2024-12-21 22:05:48 -0500

    Runtime: 27 ms (81.35%), Memory: 18.6 MB (52.59%) - Gitcode

--- 930-binary-subarrays-with-sum/930. Binary Subarrays With Sum.py
+++ 930-binary-subarrays-with-sum/930. Binary Subarrays With Sum.py
@@ -1,14 +1,34 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        total = currSum = 0
-        count = defaultdict(int)
-        count[0] = 1
+        # total = currSum = 0
+        # count = defaultdict(int)
+        # count[0] = 1
 
-        for num in nums:
-            currSum += num
-            if currSum-goal in count:
-                total += count[currSum-goal]
-            count[currSum] += 1
+        # for num in nums:
+        #     currSum += num
+        #     if currSum-goal in count:
+        #         total += count[currSum-goal]
+        #     count[currSum] += 1
+
+        def slidingWindow(target):
+            if target < 0:
+                return 0
+            count = 0
+            currSum = 0
+            left = 0
+
+            for right in range(len(nums)):
+                currSum += nums[right]
+
+                while currSum > target:
+                    currSum -= nums[left]
+                    left += 1
+                
+                count += (right-left+1)
+            print(count)
+            return count
+
+        total = slidingWindow(goal) - slidingWindow(goal - 1)
 
         return total
 
