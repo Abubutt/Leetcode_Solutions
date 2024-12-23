5f829cf570b3b349848ff5623cffe573dd8f281a
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2024-12-22 19:09:50 -0500

    Runtime: 170 ms (23.83%), Memory: 18.3 MB (7.98%) - Gitcode

--- 1657-determine-if-two-strings-are-close/1657. Determine if Two Strings Are Close.py
+++ 1657-determine-if-two-strings-are-close/1657. Determine if Two Strings Are Close.py
@@ -1,29 +1,27 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        if len(word1) != len(word2):
-            return False
-
         count1 = defaultdict(int)
         count2 = defaultdict(int)
+
+        if len(word1) != len(word2):
+            return False
 
         for i in range(len(word1)):
             count1[word1[i]] += 1
             count2[word2[i]] += 1
 
+        freq = list(count2.values())
+
         if count1 == count2:
             return True
-        
+
         if count1.keys() != count2.keys():
             return False
-        
-        values = list(count2.values())
+
         for key in count1:
-            print(values, count1[key])
-            if count1[key] not in values:
+            if count1[key] not in freq:
                 return False
-            values.remove(count1[key])
+            freq.remove(count1[key])
 
         return True
-
-        # "abbzzca". a1 b1 z0 c1
-        # "babzzcz". b1 a0 z1 c1
+        
