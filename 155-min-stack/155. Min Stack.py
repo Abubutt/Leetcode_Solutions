79cdbe5428c38e5c49bd56c5e4ae8f4513627a9e
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-01-01 00:17:22 -0500

    Runtime: 16 ms (13.21%), Memory: 21.7 MB (7.09%) - Gitcode

---
+++ 155-min-stack/155. Min Stack.py
@@ -0,0 +1,36 @@
+class MinStack:
+
+    def __init__(self):
+        self.minStack = []
+        self.minVal = float('inf')
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.minVal = min(self.minVal, val)
+        self.minStack.append(self.minVal)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack:
+            self.minStack.pop()
+            self.stack.pop()
+            if not self.stack:
+                self.minVal = float('inf')
+            else:
+                self.minVal = self.minStack[-1]
+        
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+        
+    [-10, 14, 10]
+    [-10, -10, -10]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
