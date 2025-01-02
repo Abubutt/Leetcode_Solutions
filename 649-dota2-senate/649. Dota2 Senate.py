eb13c02e22ea0eda171498772e89002c37574fb5
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-01-01 22:34:13 -0500

    Runtime: 11 ms (78.77%), Memory: 18.1 MB (14.89%) - Gitcode

---
+++ 649-dota2-senate/649. Dota2 Senate.py
@@ -0,0 +1,29 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        dq = deque()
+        rq = deque()
+        count = len(senate)
+
+        for i in range(count):
+            if senate[i] == "R":
+                rq.append(i)
+            else:
+                dq.append(i)
+
+        while dq and rq:
+            count += 1
+
+            if dq[0] < rq[0]:
+                dq.append(count)
+            else:
+                rq.append(count)
+
+            rq.popleft()
+            dq.popleft()
+
+        if dq:
+            return "Dire"
+        
+        return "Radiant"
+
+        
