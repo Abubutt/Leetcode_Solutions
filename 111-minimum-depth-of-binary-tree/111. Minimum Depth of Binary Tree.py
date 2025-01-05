1bfa63eff2eddac48e60a9041db5badd66380650
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-01-04 23:40:20 -0500

    Runtime: 47 ms (49.53%), Memory: 50.6 MB (8.08%) - Gitcode

--- 111-minimum-depth-of-binary-tree/111. Minimum Depth of Binary Tree.py
+++ 111-minimum-depth-of-binary-tree/111. Minimum Depth of Binary Tree.py
@@ -10,20 +10,19 @@ class Solution:
             return 0
         
         self.depth = float('inf')
-        
-        def dfs(node, curr):
-            if not node:
+        def dfs(root, depth):
+            if not root:
                 return
             
-            if not node.left and not node.right:
-                self.depth = min(self.depth, curr)
-                
-            if node.left:
-                dfs(node.left, curr+1)
-            
-            if node.right:
-                dfs(node.right, curr+1)
-                
+            if not root.left and not root.right:
+                self.depth = min(self.depth, depth)
+
+            if root.left:
+                dfs(root.left, depth+1)
+            if root.right:
+                right = dfs(root.right, depth+1)
+
+        
         dfs(root, 1)
         return self.depth
         
