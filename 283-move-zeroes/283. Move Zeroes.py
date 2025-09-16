615e3e2a72e3a145292fb2f7f8d3ff3b6542ef61
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-09-15 22:20:22 -0400

    Runtime: 4 ms (60.26%), Memory: 19 MB (9.83%) - Gitcode

--- 283-move-zeroes/283. Move Zeroes.py
+++ 283-move-zeroes/283. Move Zeroes.py
@@ -3,21 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-
         left = 0
-        right = 0
 
-        while right < len(nums):
-            if nums[left] == 0 and nums[right] == 0:
-                right += 1
-            elif nums[left] == 0:
+        for right in range(len(nums)):
+            if nums[right] != 0:
                 nums[left], nums[right] = nums[right], nums[left]
                 left += 1
-                right += 1
-            else:
-               left += 1
-               right += 1 
-
-            # [1, 0, 1]
-
-            # [0,1,0,3,12]
+        
