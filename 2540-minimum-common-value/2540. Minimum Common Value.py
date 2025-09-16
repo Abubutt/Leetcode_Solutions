bbde13b826327cb2d955eb5deefb6bfecb404d90
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-09-15 22:05:06 -0400

    Runtime: 26 ms (8.96%), Memory: 37.2 MB (35.37%) - Gitcode

--- 2540-minimum-common-value/2540. Minimum Common Value.py
+++ 2540-minimum-common-value/2540. Minimum Common Value.py
@@ -1,16 +1,18 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        i=j=0
-        ans = -1
+        minCommon = float('inf')
+        i = j = 0
 
         while i < len(nums1) and j < len(nums2):
-            if nums1[i] < nums2[j]:
+            if nums1[i] == nums2[j]:
+                minCommon = min(minCommon, nums1[i])
                 i += 1
-            elif nums2[j] < nums1[i]:
                 j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
             else:
-                ans = nums1[i]
-                break
-        
-        return ans
+                j += 1
+
+        return -1 if minCommon == float('inf') else minCommon
+
         
