2ea30e3ad363036f647b720cc99e5cc1471d0128
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-09-16 21:01:59 -0400

    Runtime: 0 ms (100.00%), Memory: 17.8 MB (34.32%) - Gitcode

--- 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
+++ 2000-reverse-prefix-of-word/2000. Reverse Prefix of Word.py
@@ -1,16 +1,19 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        ans = [char for char in word]
+        letters = [letter for letter in word]
+        size = len(letters)
+        
+        for right in range(size):
+            if letters[right] == ch:
+                i = 0
+                j = right
 
-        for i in range(len(word)):
-            if word[i] == ch:
-                left = 0
-                right = i
-                while left < right:
-                    ans[left], ans[right] = ans[right], ans[left]
-                    left += 1
-                    right -= 1
+                while i < j:
+                    letters[i], letters[j] = letters[j], letters[i]
+                    i += 1
+                    j -= 1
+
                 break
         
-        return "".join(ans)
+        return "".join(letters)
         
