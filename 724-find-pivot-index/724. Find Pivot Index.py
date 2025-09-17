c81b4c906b0b655865145ac697aab86d9ecdd928
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-09-16 21:38:38 -0400

    Runtime: 7 ms (56.75%), Memory: 18.8 MB (38.68%) - Gitcode

--- 724-find-pivot-index/724. Find Pivot Index.py
+++ 724-find-pivot-index/724. Find Pivot Index.py
@@ -1,31 +1,12 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        prefixSum = [nums[0]]
-        pivotIndex = -1
-        size = len(nums)
-        totalSum = nums[0]
+        currSum = 0
+        totalSum = sum(nums)
 
-        for i in range(1, size):
-            totalSum += nums[i]
-            prefixSum.append(nums[i] + prefixSum[-1])
-        
-        for i in range(size):
-            leftSum = 0
-            rightSum = 0
+        for i in range(len(nums)):
+            if currSum == totalSum - currSum - nums[i]:
+                return i
 
-            if i > 0:
-                leftSum = prefixSum[i-1]
-            
-            if i < size:
-                rightSum = totalSum - prefixSum[i]
+            currSum += nums[i]
 
-            if leftSum == rightSum:
-                pivotIndex = i
-                break
-
-        return pivotIndex
-
-
-
-        
-        
+        return -1
