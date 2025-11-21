cfcd6d291d19694bf53560c2a3a73e372fe5c7f3
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-11-20 23:17:25 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (37.43%) - Gitcode

---
+++ 118-pascals-triangle/118. Pascal's Triangle.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        dp = [[1] * i for i in range(1, numRows + 1)]
+
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[row]) - 1):
+                if col - 1 >= 0:
+                    dp[row][col] = dp[row-1][col-1] + dp[row-1][col]
+
+        return dp
+        
+
+    
+    [1],
+    [1,1],
+    [1,2,1],
+    [1,1,1,1],
+    [1,1,1,1,1]
+    
+  
