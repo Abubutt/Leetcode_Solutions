7bfc190dfa453c062d43b7aa1d306c90101b311e
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-11-20 23:18:03 -0500

    Runtime: 0 ms (100.00%), Memory: 17.9 MB (16.03%) - Gitcode

--- 118-pascals-triangle/118. Pascal's Triangle.py
+++ 118-pascals-triangle/118. Pascal's Triangle.py
@@ -4,17 +4,8 @@ class Solution:
 
         for row in range(1, len(dp)):
             for col in range(1, len(dp[row]) - 1):
-                if col - 1 >= 0:
-                    dp[row][col] = dp[row-1][col-1] + dp[row-1][col]
+                dp[row][col] = dp[row-1][col-1] + dp[row-1][col]
 
         return dp
-        
-
-    
-    [1],
-    [1,1],
-    [1,2,1],
-    [1,1,1,1],
-    [1,1,1,1,1]
     
   
