873f864032ab27cfa9fd6f27593519e40dbf45f1
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2025-12-02 18:11:28 -0500

    Runtime: 6 ms (58.95%), Memory: 18 MB (77.44%) - Gitcode

--- 47-permutations-ii/47. Permutations II.py
+++ 47-permutations-ii/47. Permutations II.py
@@ -1,23 +1,28 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
         def backTrack(curr, seen):
             if len(curr) == len(nums):
-                ans.add(tuple(curr[:]))
+                ans.append(curr[:])
                 return
 
             
             for i in range(len(nums)):
-                if i not in seen:
-                    curr.append(nums[i])
-                    seen.add(i)
-                    backTrack(curr, seen)
-                    curr.pop()
-                    seen.remove(i)
+                if i in seen:
+                    continue
+                if i > 0 and nums[i-1] == nums[i] and i-1 not in seen:
+                    continue
+   
+                curr.append(nums[i])
+                seen.add(i)
+                backTrack(curr, seen)
+                curr.pop()
+                seen.remove(i)
 
-        ans = set()
+        ans = []
         backTrack([], set())
 
-        return list(ans)
+        return ans
 
         # b([], 0)
 
