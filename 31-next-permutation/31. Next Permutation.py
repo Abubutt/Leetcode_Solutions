91c946c61600e4570946726fe8e7c30019375f0c
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2026-02-21 18:40:30 -0500

    Runtime: 0 ms (100.00%), Memory: 19.4 MB (54.38%) - Gitcode

---
+++ 31-next-permutation/31. Next Permutation.py
@@ -0,0 +1,27 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        pivot = -1
+
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                pivot = i
+                break
+
+        if pivot == -1:
+            nums.reverse()
+            return
+
+        index = None
+
+        for i in range(len(nums) - 1, pivot, -1):
+            if nums[i] > nums[pivot]:
+                index = i
+                break
+
+        nums[pivot], nums[index] = nums[index], nums[pivot]
+        nums[pivot + 1:] = reversed(nums[pivot + 1:])
+
+
+        
+
+        
