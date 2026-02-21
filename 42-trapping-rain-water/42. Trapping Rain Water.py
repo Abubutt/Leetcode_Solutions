d68a33f5bb55b2fba7542dcbf8ff9d72450069b9
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2026-02-20 22:08:22 -0500

    Runtime: 16 ms (27.47%), Memory: 21.2 MB (37.66%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,18 +1,23 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height)-1
-        maxLeft, maxRight = height[l], height[r]
-        totalTrapped = 0
+        maxL = [0] * len(height)
+        maxR = [0] * len(height)
+
+        currMax = 0
+        for i in range(len(height)):
+            maxL[i] = currMax
+            currMax = max(currMax, height[i])
+
+        currMax = 0
+        for i in range(len(height) - 1, -1, -1):
+            maxR[i] = currMax
+            currMax = max(currMax, height[i])
+
+        ans = 0
+
+        for i in range(len(height)):
+            amount = max(0, min(maxL[i], maxR[i]) - height[i])
+            ans += amount
+
+        return ans
         
-        while l<r:
-            if maxLeft < maxRight:
-                l+=1
-                maxLeft = max(maxLeft, height[l])
-                totalTrapped+=(maxLeft-height[l])
-                #maxLeft = max(maxLeft, height[l])
-            else:
-                r-=1
-                maxRight = max(maxRight, height[r])
-                totalTrapped+=(maxRight-height[r])
-                #maxRight = max(maxRight, height[r])
-        return totalTrapped
