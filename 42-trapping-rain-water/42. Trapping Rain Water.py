442307281b2fc623f409d31962d6eb6d463882f8
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2026-02-20 22:18:38 -0500

    Runtime: 0 ms (100.00%), Memory: 21.2 MB (37.66%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,23 +1,38 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        maxL = [0] * len(height)
-        maxR = [0] * len(height)
+        # maxL = [0] * len(height)
+        # maxR = [0] * len(height)
 
-        currMax = 0
-        for i in range(len(height)):
-            maxL[i] = currMax
-            currMax = max(currMax, height[i])
+        # currMax = 0
+        # for i in range(len(height)):
+        #     maxL[i] = currMax
+        #     currMax = max(currMax, height[i])
 
-        currMax = 0
-        for i in range(len(height) - 1, -1, -1):
-            maxR[i] = currMax
-            currMax = max(currMax, height[i])
+        # currMax = 0
+        # for i in range(len(height) - 1, -1, -1):
+        #     maxR[i] = currMax
+        #     currMax = max(currMax, height[i])
 
-        ans = 0
+        # ans = 0
 
-        for i in range(len(height)):
-            amount = max(0, min(maxL[i], maxR[i]) - height[i])
-            ans += amount
+        # for i in range(len(height)):
+        #     amount = max(0, min(maxL[i], maxR[i]) - height[i])
+        #     ans += amount
 
-        return ans
+        # return ans
+
+        left = ans = 0
+        right = len(height) - 1
+        maxL = maxR = 0
+
+        while left < right:
+            if height[left] < height[right]:
+                maxL = max(maxL, height[left])
+                ans += maxL - height[left]
+                left += 1
+            else:
+                maxR = max(maxR, height[right])
+                ans += maxR - height[right]
+                right -= 1
         
+        return ans
