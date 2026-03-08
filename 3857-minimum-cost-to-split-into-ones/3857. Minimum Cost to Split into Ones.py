1fa0d7f2e3c2a46576354a65008e9913d4b6d7f7
Abu Butt <62613696+Abubutt@users.noreply.github.com>
2026-03-07 22:00:38 -0500

    Runtime: 1631 ms (7.60%), Memory: 19.3 MB (65.51%) - Gitcode

--- 3857-minimum-cost-to-split-into-ones/3857. Minimum Cost to Split into Ones.py
+++ 3857-minimum-cost-to-split-into-ones/3857. Minimum Cost to Split into Ones.py
@@ -1,20 +1,35 @@
 class Solution:
     def minCost(self, n: int) -> int:
-        memo = {}
-        def dp(n):
-            if n in memo:
-                return memo[n]
+        # memo = {}
+        # def dp(n):
+        #     if n in memo:
+        #         return memo[n]
 
-            if n <= 1:
-                return 0
+        #     if n <= 1:
+        #         return 0
 
+        #     minVal = float('inf')
+
+        #     for i in range(1, n):
+        #         j = n - i
+        #         minVal = min(minVal, (i * j) + dp(i) + dp(j))
+
+        #     memo[n] = minVal
+        #     return minVal
+
+        # return dp(n)
+
+        dp = [float('inf') for _ in range(n + 1)]
+        dp[0] = 0
+        dp[1] = 0
+
+        for i in range(2, n + 1):
             minVal = float('inf')
 
-            for i in range(1, n):
-                j = n - i
-                minVal = min(minVal, (i * j) + dp(i) + dp(j))
-                
-            memo[n] = minVal
-            return minVal
+            for j in range(1, i):
+                k = i - j
+                minVal = min(minVal, (j * k) + dp[j] + dp[k])
 
-        return dp(n)
+            dp[i] = minVal
+
+        return dp[n]
